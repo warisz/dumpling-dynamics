bc11d426f5fe33781e17b8a9ebf8f46fa7d8bc1c
Waris Z <nusratzahoor@v1041-wn-rt-b-149-29.campus-dynamic.uwaterloo.ca>
2021-09-18 18:03:19 -0400

    working serial with pyqt5 start

--- serial/serial.ino
+++ serial/serial.ino
@@ -1,14 +1,16 @@
 
 #include <Servo.h>
+#define LED 13
 
 Servo myservo;  
 char data;
 int pos = 0;
 
 void setup() {
-  myservo.attach(9);  
+  myservo.attach(9);
   Serial.begin(9600);
   myservo.write(0);
+  pinMode(LED, OUTPUT);
 }
 
 void loop() {
@@ -18,11 +20,15 @@ void loop() {
     
     if(data == 'a'){
       myservo.write(90);
-       }
+      digitalWrite(LED, HIGH);
+      delay(20);
       Serial.write('a');
+       }
+
       
     }else if(data == 'b'){
       myservo.write(180);
+      digitalWrite(LED, LOW);
       delay(20);
       Serial.write('b');
       
